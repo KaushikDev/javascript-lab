eefde8964a079b9fb6ea94430ce81ef9be0c94ba
kaushik3pg <piyush.kaushik@3pillarglobal.com>
2026-03-17 23:45:08 +0530

    added canvas

---
+++ modules/canvas/canvas.js
@@ -0,0 +1,21 @@
+export function launchSpaceShooter(interactivePanelPlayArea) {
+  const canvas = document.createElement("canvas");
+  const parentDiv = document.createElement("div");
+
+  parentDiv.classList.add("ip__canvas-container");
+  interactivePanelPlayArea.appendChild(parentDiv);
+
+  parentDiv.appendChild(canvas);
+  canvas.classList.add("ip__canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillRect(0, 0, 400, 400);
+  createShip(ctx);
+}
+
+function createShip(ctx) {
+  ctx.beginPath();
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.arc(200, 200, 50, 0, Math.PI * 2, true);
+  ctx.fill();
+}
